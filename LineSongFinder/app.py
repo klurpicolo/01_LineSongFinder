a7828969c3eb419a2626a935ccea3900599899d4
warit <warit.boonmasiri@gmail.com>
2019-01-05 19:28:17 +0700

    add __init__ to make it as package

---
+++ LineSongFinder/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, abort
+from . import line_api
+import json
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World! Fuck You Papol"
+
+
+@app.route("/about")
+def about():
+    return "<h1>Here is about page</h1>"
+
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Print pretty Json
+        request_str = json.dumps(request.json, indent=4)
+        print(request_str)
+
+        line_api.reply_echo_msg(request)
+
+        return '', 200
+    else:
+        about(400)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=8080)
---
+++ LineSongFinder/line_api.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+# Setup parameter for Line API
+url = "https://api.line.me/v2/bot/message/reply"
+
+querystring = {"Content-Type": "application/json", "Authorization": "%20Bearer%201hFvFhSffFNku6B/E0Yt+7a7yNF2xLU0/yMz5SfoEUBsnLX1JddQHXg2q0lWCXYnsjjFTi85oqD0DRsVre3UgombcFA+8ZfpSDbWQRjZ6Qx3wNwLt8uPP5uwFwpjP6X2uE9Mav01WQcxDAT9gx7+2AdB04t89/1O/w1cDnyilFU="}
+
+reply_payload = {
+    "replyToken": "xxxxxxxx",
+                  "messages": [
+                      {
+                          "type": "text",
+                          "text": "This is reply_echo_msh: "
+                      }
+                  ]
+}
+
+reply_msg = {
+    "type": "text",
+    "text": "This is reply_echo_msh: "
+}
+
+headers = {
+    'Content-Type': "application/json",
+    'Authorization': "Bearer 1hFvFhSffFNku6B/E0Yt+7a7yNF2xLU0/yMz5SfoEUBsnLX1JddQHXg2q0lWCXYnsjjFTi85oqD0DRsVre3UgombcFA+8ZfpSDbWQRjZ6Qx3wNwLt8uPP5uwFwpjP6X2uE9Mav01WQcxDAT9gx7+2AdB04t89/1O/w1cDnyilFU=",
+    'cache-control': "no-cache",
+    'Postman-Token': "33629a0d-e4e1-4e75-bd2d-db1a37cf2377"
+}
+#######################################################
+
+
+def echo_msg(request):
+    events = request.json["events"]
+
+    for event in events:
+        print(event["type"])
+        print(event["replyToken"])
+
+
+def reply_echo_msg(request):
+    events = request.json["events"]
+
+    for event in events:
+        reply_token = event["replyToken"]
+        if event["message"]["type"] == "text":
+            msg_text = event["message"]["text"]
+        else:
+            msg_text = "This isn't text!"
+
+    # reply_payload
+    reply_msg["text"] = "This is reply_echo_msh: " + msg_text
+    reply_msgs = [reply_msg]
+    # Set reply message
+    reply_payload["replyToken"] = reply_token
+    reply_payload["messages"] = reply_msgs
+
+    print(reply_payload)
+
+    response = requests.request("POST", url, data=json.dumps(reply_payload), headers=headers, params=querystring)
+    print(response.text)
+    print(response.status_code)
